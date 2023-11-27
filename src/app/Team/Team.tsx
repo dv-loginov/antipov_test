f6dcdeead099cb8c91c4e69555ae1eb00b86d76f
dv-loginov <dilog@rambler.ru>
2023-11-27 16:30:42 +0300

    Удалить не используемый код

--- src/app/Team/Team.tsx
+++ src/app/Team/Team.tsx
@@ -3,31 +3,15 @@ import './Team.scss';
 import Header from "../../components/Header/Header";
 // import Card from "../../components/Card/Card";
 import NextButton from "../../components/NextButton/NextButton";
-// import {getToken, getUsers} from "../../api/api";
 import {useTypedSelector} from "../../hooks/useTypedSelector";
-// import {useDispatch} from "react-redux";
-// import {fetchUsers} from "../../store/actions-creators/user";
 import {useActions} from "../../hooks/useActions";
 
 const Team: React.FC = () => {
 
     const {users, error, loading} = useTypedSelector(state => state.user);
-    // console.log(users);
-    // const dispatch = useDispatch();
     const {fetchUsers} = useActions();
 
     useEffect(() => {
-        // getUsers()
-        //     .then((data) => {
-        //         console.log(data);
-        //     })
-        //     .catch((err) => console.error(err));
-        //
-        // getToken()
-        //     .then((data) => {
-        //         console.log(data);
-        //     })
-        //     .catch((err) => console.error(err));
         fetchUsers();
     },[]);
 
